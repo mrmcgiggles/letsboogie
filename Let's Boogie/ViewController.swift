a25948588601f60b57508b29ea3589aab3b5bc16
AJ Lodge <ahhjlodge@gmail.com>
2017-03-15 12:47:46 -0700

    1st commit

--- Let's Boogie/ViewController.swift
+++ Let's Boogie/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            mainLabel.text = "PUSSY BOI DID IT 10 TIMEZ AND DEN SUM"
+        }
+    }
+    @IBAction func buttonTapped2(_ sender: Any) {
+        mainLabel.text = "git it Pussyboi did git it"
+        print("2nd Button Tapped")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
